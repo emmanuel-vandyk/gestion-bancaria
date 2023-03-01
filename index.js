1fc912d215fcb3d1385e22757cf8b4041d747697
Emmanuel-VanDick9 <emmaaavd@gmail.com>
2023-03-01 10:09:22 -0300

    Agregar metodo de deposito en cuentaCorriente

--- index.js
+++ index.js
@@ -6,32 +6,52 @@ const saldoCuenta = 1000;
 class Cliente { 
     nombreCliente;
     dniCliente;
-    numeroCuenta;
-    saldoCuenta;
     rutCliente;
 }
 
+class cuentaCorriente {
+    saldoCuenta;
+    numeroCuenta;
+    agencia;
+
+    depositoEnCuenta(valor) {
+        this.saldoCuenta += valor;
+    }
+}
+
+// Cliente y Cuenta 1
 const cliente1 = new Cliente();
 
 cliente1.nombreCliente = "José";
 cliente1.dniCliente = "13232";
-cliente1.numeroCuenta = "232323";
-cliente1.saldoCuenta = 13000;
+cliente1.rutCliente = "V13804005";
 
+const cuentaCorriente1 = new cuentaCorriente();
+cuentaCorriente1.numeroCuenta = "232323";
+cuentaCorriente1.saldoCuenta = 13000;
+cuentaCorriente1.agencia = 1001;
+
+//Cliente y Cuenta 2
 const cliente2 = new Cliente();
 
 cliente2.nombreCliente = "Juan";
 cliente2.dniCliente = "3457521";
-cliente2.numeroCuenta = "765331";
-cliente2.saldoCuenta = 20000;
+cliente2.rutCliente = "V11225892";
 
-const cliente3 = new Cliente();
 
-cliente3.nombreCliente = "María";
-cliente3.dniCliente = "1234567";
-cliente3.numeroCuenta = "986743";
-cliente3.saldoCuenta = 30000;
+const cuentaCorriente2 = new cuentaCorriente();
+cuentaCorriente2.numeroCuenta = "46676565";
+cuentaCorriente2.saldoCuenta = 30000;
+cuentaCorriente2.agencia = 1002;
 
-console.log(cliente1);
-console.log(cliente2);
-console.log(cliente3);
+console.log(cuentaCorriente2.saldoCuenta);
+cuentaCorriente2.depositoEnCuenta(100)
+console.log(cuentaCorriente2.saldoCuenta);
+cuentaCorriente2.depositoEnCuenta(200);
+console.log(cuentaCorriente2.saldoCuenta);
+cuentaCorriente2.depositoEnCuenta(500);
+console.log(cuentaCorriente2.saldoCuenta);
+
+
+
+
